0c5d86b07062bf0b22f89038385492202426d741
PhebianOdufuwa <odufuwaphebian@gmail.com>
2020-10-16 22:36:57 -0600

    "Updated by Temi"

--- Sample.java
+++ Sample.java
@@ -3,6 +3,13 @@ public class Sample{
 public static void main(String[] args) {
 
     System.out.println("My name is Google");
+    
+    System.out.println(mainMethod(2,5));
        
    } 
+   public int mainMethod(a, b){
+
+    return a*b;
+
+   }
 }
